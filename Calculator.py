818867d7add383fbcb74d136e14f3378afc84fc8
Wolfman288 <45688865+Wolfman288@users.noreply.github.com>
2020-11-23 12:44:46 +0100

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,3 +1,5 @@
+###Emanuel Näslun###
+
 def calculate():
     operation = input('''
 Skriv in den Matte operatör du vill använda:
@@ -7,21 +9,26 @@ Skriv in den Matte operatör du vill använda:
 / för division
 ''')
 
+    #Dem 2 nummer du vill använda
     number_1 = int(input('Skriv in första numret: '))
     number_2 = int(input('Skriv in andra numret: '))
 
+    #Addition
     if operation == '+':
         print('{} + {} = '.format(number_1, number_2))
         print(number_1 + number_2)
 
+    #Subtraction
     elif operation == '-':
         print('{} - {} = '.format(number_1, number_2))
         print(number_1 - number_2)
 
+    #Multiplication
     elif operation == '*':
         print('{} * {} = '.format(number_1, number_2))
         print(number_1 * number_2)
 
+    #Division
     elif operation == '/':
         print('{} / {} = '.format(number_1, number_2))
         print(number_1 / number_2)
@@ -31,13 +38,13 @@ Skriv in den Matte operatör du vill använda:
 
     
     again()
-
+#Om du vill göra igen
 def again():
     calc_again = input('''
 Vill du använda mig igen?
 Skriv J för JA eller N för NEJ.
 ''')
-
+#Ja eller Nej
     if calc_again.upper() == 'J':
         calculate()
     elif calc_again.upper() == 'N':
